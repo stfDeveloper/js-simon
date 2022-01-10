97e8635e081761d725eafe3f3dd6c84122a00279
stfDeveloper <sstefancosmin@icloud.com>
2022-01-10 09:18:49 +0100

    commento con numeri indovinati

--- myscript.js
+++ myscript.js
@@ -4,4 +4,17 @@ let numbers = [];
     numbers.push(random);
     document.getElementById("game").innerHTML="Memorizza questi numeri <br>" +numbers;
  }
+ setTimeout (tempo, 3000);
+ function tempo(){
+     game.innerHTML = "";
+     let array2 = [];
+     let numeroUtente;
 
+     for (let index = 0; index < numbers.length; index++) {
+         numeroUtente = parseInt (prompt("Inserisci i numeri visti in ordine"));
+         if (numbers.includes(numeroUtente)) {
+             array2.push(numeroUtente)
+         }
+     }game.innerHTML
+      =`Hai indovinato ${array2.length} di questi numeri :${array2.join(' ')}`;
+ }
